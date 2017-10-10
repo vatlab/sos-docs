b830c62732331d8e17229d4503fe1685a0be9b91
Ma <jma7@BSTMC07TP062G1J.local>
2017-10-10 12:07:27 -0500

    comment out tabs

--- js/sos_script.js
+++ js/sos_script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$("#intro_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Overview.html");
-	$("#installation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Running_SoS.html");
-	$("#documentation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Documentation.html");
-	$("#features_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Features.html");
+	//$("#installation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Running_SoS.html");
+	//$("#documentation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Documentation.html");
+	//$("#features_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Features.html");
 /*	$("#intro_content").load("doc/homepage/Overview.html"); 
 	$("#installation_content").load("doc/homepage/Running_SoS.html"); 
 	$("#documentation_content").load("doc/homepage/Documentation.html"); 
