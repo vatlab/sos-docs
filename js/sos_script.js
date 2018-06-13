e85f089e9f6293cd6f601e5b2a224e641d2e0442
Bo Peng <ben.bog@gmail.com>
2018-06-13 11:20:23 -0500

    Stop reading homepage from github raw

--- js/sos_script.js
+++ js/sos_script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-	$("#intro_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Overview.html");
-	$("#features_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Features.html");
-	$("#installation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Running_SoS.html");
-	$("#notebook_documentation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Notebook_Documentation.html");
-	$("#workflow_documentation_content").load("https://raw.githubusercontent.com/vatlab/sos-docs/master/Workflow_Documentation.html");
+	$("#intro_content").load("https://vatlab.github.io/sos-docs/Overview.html");
+	$("#features_content").load("https://vatlab.github.io/sos-docs/Features.html");
+	$("#installation_content").load("https://vatlab.github.io/sos-docs/Running_SoS.html");
+	$("#notebook_documentation_content").load("https://vatlab.github.io/sos-docs/Notebook_Documentation.html");
+	$("#workflow_documentation_content").load("https://vatlab.github.io/sos-docs/Workflow_Documentation.html");
 	
 	// var tuts=tutorials
 	// $("#tutorial > .container").append('<div class="row">')
