b0bcb5733afdb566def027e6c64e4a13cabf12c8
Bo Peng <ben.bog@gmail.com>
2018-06-26 16:46:39 -0500

    Adjust TOC location

--- js/sos_script.js
+++ js/sos_script.js
@@ -1,106 +1,114 @@
 $(document).ready(function () {
-	$("#intro_content").load("Overview.html");
-	$("#features_content").load("Features.html");
-	$("#installation_content").load("Running_SoS.html");
-	$("#notebook_documentation_content").load("Notebook_Documentation.html");
-	$("#workflow_documentation_content").load("Workflow_Documentation.html");
-	
-	// var tuts=tutorials
-	// $("#tutorial > .container").append('<div class="row">')
-	// for (var a =0;a<tuts.length;a++){
-	// 	var name =tuts[a];
-	// 	var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
- //        				+'<div class="portfolio-caption">';
-	// 	oneString+='<a href="./doc/tutorials/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
-	// 	oneString+='</div></div>';       	
-	// 	 $("#tutorial > .container").append(
-	//        	oneString
-	//      );
-	// }	
-	// $("#tutorial > .container").append('</div>');	
+    $("#intro_content").load("Overview.html");
+    $("#features_content").load("Features.html");
+    $("#installation_content").load("Running_SoS.html");
+    $("#notebook_documentation_content").load("Notebook_Documentation.html");
+    $("#workflow_documentation_content").load("Workflow_Documentation.html");
+
+    // var tuts=tutorials
+    // $("#tutorial > .container").append('<div class="row">')
+    // for (var a =0;a<tuts.length;a++){
+    //     var name =tuts[a];
+    //     var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
+ //                        +'<div class="portfolio-caption">';
+    //     oneString+='<a href="./doc/tutorials/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
+    //     oneString+='</div></div>';
+    //      $("#tutorial > .container").append(
+    //            oneString
+    //      );
+    // }
+    // $("#tutorial > .container").append('</div>');
 
 
- // 	var docs=documentation
-	// $("#documentation > .container").append('<div class="row">')
-	// for (var a =0;a<docs.length;a++){
-	// 	var name =docs[a];
-	// 	var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
- //        				+'<div class="portfolio-caption">';
-	// 	oneString+='<a href="./doc/documentation/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
-	// 	oneString+='</div></div>';       	
-	// 	 $("#documentation > .container").append(
-	//        	oneString
-	//      );
-	// }	
-	// $("#documentation > .container").append('</div>');		
+ //     var docs=documentation
+    // $("#documentation > .container").append('<div class="row">')
+    // for (var a =0;a<docs.length;a++){
+    //     var name =docs[a];
+    //     var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
+ //                        +'<div class="portfolio-caption">';
+    //     oneString+='<a href="./doc/documentation/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
+    //     oneString+='</div></div>';
+    //      $("#documentation > .container").append(
+    //            oneString
+    //      );
+    // }
+    // $("#documentation > .container").append('</div>');
 
 
 
-	 $(window).on('scroll',function() {
+     $(window).on('scroll',function() {
             var scrolltop = $(this).scrollTop();
-         
-            if(scrolltop >= 600) {
+
+            if(scrolltop >= 420) {
               $('#fixedbar').fadeIn(250);
+              $('.toc').css({
+                  "position": "fixed",
+                  "top": "105px"
+              });
             }
-            
-            else if(scrolltop <= 600) {
+
+            else if(scrolltop <= 420) {
               $('#fixedbar').fadeOut(250);
+              $(".toc").css({
+                  "position": "relative",
+                  "top": "0px"
+              });
             }
           });
 
-	// var tabindex=$("#fixedbar").tabs({active:1}).tabs("option","active");
-	// console.log(tabindex);
-	// $("#navigation").tabs().tabs("option","active",tabindex);
+    // var tabindex=$("#fixedbar").tabs({active:1}).tabs("option","active");
+    // console.log(tabindex);
+    // $("#navigation").tabs().tabs("option","active",tabindex);
 
-	  $("#fixedbar li a ").click(function(){
-	  		var tabindex=$(this).attr('href');
-	  		$('#navigation li a').filter('[href="'+tabindex+'"]').tab('show'); 		
-	  })
-        
+      $("#fixedbar li a ").click(function(){
+              var tabindex=$(this).attr('href');
+              $('#navigation li a').filter('[href="'+tabindex+'"]').tab('show');
+      })
+
       $("#navigation li a ").click(function(){
-	  		var tabindex=$(this).attr('href');
-	  		$('#fixedbar li a').filter('[href="'+tabindex+'"]').tab('show'); 		
-	  })
-	  var imgs=images
+              var tabindex=$(this).attr('href');
+              $('#fixedbar li a').filter('[href="'+tabindex+'"]').tab('show');
+      })
+      var imgs=images
 
       $('header').css({'background-image': 'url(img/' + imgs[Math.floor(Math.random() * imgs.length)] + ')'});
 
      $('.nav a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		})  
-    
-	  // $('a.scroll').on('click', function (e) {
-	  //       var href = $(this).attr('href');
-	  //       $('html, body').animate({
-	  //           scrollTop: $(href).offset().top-50
-	  //       }, 'slow');
-	  //       e.preventDefault();
-	  //  });
+          e.preventDefault()
+          $(this).tab('show')
+        })
 
-	  function activateTab(tab){
-    		$('.nav-tabs a[href="' + tab + '"]').tab('show');
-	  };
+      // $('a.scroll').on('click', function (e) {
+      //       var href = $(this).attr('href');
+      //       $('html, body').animate({
+      //           scrollTop: $(href).offset().top-50
+      //       }, 'slow');
+      //       e.preventDefault();
+      //  });
 
-	  var hash = document.location.hash;	
-	  // if (hash=="#documentation") {
-	  // 		activateTab("#4");
-	  // 		$('html, body').stop().animate({
-   //          	scrollTop: ($("#exTab2").offset().top - 50)
-   //       	}, 'fast');
-	  // } else{
-	  // 		activateTab(hash)
-	  // }
-	  activateTab(hash)
+      function activateTab(tab){
+            $('.nav-tabs a[href="' + tab + '"]').tab('show');
+      };
+
+      var hash = document.location.hash;
+      // if (hash=="#documentation") {
+      //         activateTab("#4");
+      //         $('html, body').stop().animate({
+   //              scrollTop: ($("#exTab2").offset().top - 50)
+   //           }, 'fast');
+      // } else{
+      //         activateTab(hash)
+      // }
+      activateTab(hash)
 
 
-	  $('.nav-tabs a').on('shown.bs.tab', function (e) {
-    		var id = $(e.target).attr("href").substr(1);
-  			window.location.hash = id;
+      $('.nav-tabs a').on('shown.bs.tab', function (e) {
+            var id = $(e.target).attr("href").substr(1);
+              window.location.hash = id;
 
-		});
+        });
 
-	
+
 });
 
 //var options = [
