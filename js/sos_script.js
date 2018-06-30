4fa7ac92fe18298ebc7b8741cfe8e944c58ef52d
Bo Peng <ben.bog@gmail.com>
2018-06-29 21:18:16 -0500

    Make top row anchor link work

--- js/sos_script.js
+++ js/sos_script.js
@@ -8,51 +8,51 @@ $(document).ready(function () {
     var headerheight = $('header').height();
     var navheight = $('#navigation').height();
 
-     $(window).on('scroll',function() {
-        var scrolltop = $(this).scrollTop();
-        if(scrolltop >= headerheight) {
-          $('#navigation').addClass('sticky');
-          $('.tab-content').addClass('sticky-content');
-        } else {
-          // $('.banner').show();
-          $('#navigation').removeClass('sticky');
-          $('.tab-content').removeClass('sticky-content');
-        }
-	    });
-
-      $("#navigation li a ").click(function(){
-          var tabindex=$(this).attr('href');
-          $('#navigation li a').filter('[href="'+tabindex+'"]').tab('show');
-          $('html,body').animate({ scrollTop: headerheight + navheight }, "slow");
-          if ($('#toc_Running_SoS').length > 0) {
-            $('#toc_Running_SoS').detach().appendTo('#dropdown_runningsos');
-          }
-          if ($('#toc_Notebook_Documentation').length > 0) {
-            $('#toc_Notebook_Documentation').detach().appendTo('#dropdown_notebook_documentation');
-          }
-          if ($('#toc_Workflow_Documentation').length > 0) {
-            $('#toc_Workflow_Documentation').detach().appendTo('#dropdown_workflow_documentation');
-          }
-      })
-
-      $('#exTab2').tabs({});
-      $("#introduction").fadeIn();
-
-      var imgs=images
-      $('header').css({'background-image': 'url(img/' + imgs[Math.floor(Math.random() * imgs.length)] + ')'});
-
-      // rotating title
-      var titles = [
-          ["Polyglot Notebook"],
-          ["Workflow System"],
-      ];
-      var holder = $("#rotating_title");
-      var currentIndex = 0;
-      function rotate() {
-          holder.html(titles[currentIndex][0]);
-          currentIndex = (currentIndex + 1) % titles.length;
-          setTimeout(rotate, 2000);
+    $(window).on('scroll',function() {
+      var scrolltop = $(this).scrollTop();
+      if(scrolltop >= headerheight) {
+        $('#navigation').addClass('sticky');
+        $('.tab-content').addClass('sticky-content');
+      } else {
+        // $('.banner').show();
+        $('#navigation').removeClass('sticky');
+        $('.tab-content').removeClass('sticky-content');
       }
+	});
 
-      rotate();
+    $("#navigation li a ").click(function(){
+      var tabindex=$(this).attr('href');
+      $('#navigation li a').filter('[href="'+tabindex+'"]').tab('show');
+      $('html,body').animate({ scrollTop: headerheight + navheight }, "slow");
+      if ($('#toc_Running_SoS').length > 0) {
+        $('#toc_Running_SoS').detach().appendTo('#dropdown_runningsos');
+      }
+      if ($('#toc_Notebook_Documentation').length > 0) {
+        $('#toc_Notebook_Documentation').detach().appendTo('#dropdown_notebook_documentation');
+      }
+      if ($('#toc_Workflow_Documentation').length > 0) {
+        $('#toc_Workflow_Documentation').detach().appendTo('#dropdown_workflow_documentation');
+      }
+    })
+
+    $('#exTab2').tabs();
+	//$("#introduction").fadeIn();
+
+    var imgs=images
+    $('header').css({'background-image': 'url(img/' + imgs[Math.floor(Math.random() * imgs.length)] + ')'});
+
+    // rotating title
+    var titles = [
+      ["Polyglot Notebook"],
+      ["Workflow System"],
+    ];
+    var holder = $("#rotating_title");
+    var currentIndex = 0;
+    function rotate() {
+      holder.html(titles[currentIndex][0]);
+      currentIndex = (currentIndex + 1) % titles.length;
+      setTimeout(rotate, 2000);
+    }
+
+    rotate();
 });
