d81e8cb3526b1b05d3bf78d7c65898f7a0aee8dc
Bo Peng <ben.bog@gmail.com>
2019-02-13 13:59:18 -0600

    Remove the sh script with a python script

---
+++ development/jupyterhub/docker-sos-docs/create-pr.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import subprocess
+import string
+import random
+import pexpect
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
+
+def push_changes():
+# commit the changes
+    child = pexpect.spawn(f'git push origin {BRANCH}', cwd=DIR)
+    while True:
+        prompt = child.expect([
+            "Username for 'https://github.com':",
+            "Password for 'https://sosdocs@github.com':",
+            pexpect.EOF]
+        )
+        if prompt == 0:
+            child.sendline('sosdocs')
+        elif prompt == 1:
+            child.sendline('PASSWORD')
+        elif prompt == 2:
+            child.close()
+            break
+
+def create_pull_request():
+    child = pexpect.spawn(f'hub pull-request --base vatlab/sos-docs:master -m "sos-docs updated from sos live server"',
+        child.logfile = sys.stdout, cwd=DIR)
+    while True:
+        prompt = child.expect([
+            "github.com username: ",
+            "github.com password for sosdocs (never stored): ",
+            pexpect.EOF]
+        )
+        if prompt == 0:
+            child.sendline('sosdocs')
+        elif prompt == 1:
+            child.sendline('PASSWORD')
+        elif prompt == 2:
+            child.close()
+            break
+    print(child.before.decode())
+
+if __name__ == '__main__':
+    DIR = '/home/jovyan/sos-docs'
+    BRANCH = id_generator(6)
+
+    print('Syncing with upstream master...')
+    subprocess.call('git fetch upstream master', cwd=DIR, shell=True)
+    subprocess.call(f'git checkout -b {BRANCH} FETCH_HEAD', cwd=DIR, shell=True)
+
+    # get a list of changed files
+    out = subprocess.check_output('git diff --name-only', cwd=DIR, shell=True).decode()
+
+    # out should be something like
+    #
+    # src/user_guide/whatever.ipynb
+    for file in out.splitlines():
+        if file.startswith('src/') and file.endswith('.ipynb'):
+            dest = file.replace('src', 'doc').replace('.ipynb', '.html')
+            print(f'Converting {file} to {dest}')
+            subprocess.call(f'sos convert {DIR}/{file} {DIR}/{dest}  --template {DIR}/src/templates/sos-doc-with-banner.tpl',
+                cwd=DIR, shell=True)
+
+    subprocess.call(['git', 'commit', '.', '-m', 'Update {" ".join(out.splitlines())}'],
+        cwd=DIR)
+
+    push_changes()
+    create_pull_request()
+
