d1222c55c850f9c4a206bc470ec7081bb93f463e
Bo Peng <ben.bog@gmail.com>
2019-02-13 14:34:20 -0600

    Update create-pr script

--- development/jupyterhub/docker-sos-docs/create-pr.py
+++ development/jupyterhub/docker-sos-docs/create-pr.py
@@ -9,7 +9,8 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
    return ''.join(random.choice(chars) for _ in range(size))
 
 def push_changes():
-# commit the changes
+    # commit the changes
+    print(f'\n\nPushing branch {BRANCH}\n')
     child = pexpect.spawn(f'git push origin {BRANCH}', cwd=DIR)
     while True:
         prompt = child.expect([
@@ -24,21 +25,24 @@ def push_changes():
         elif prompt == 2:
             child.close()
             break
+    print(child.before.decode())
 
 def create_pull_request():
+    print('\n\nCreating pull request\n')
     child = pexpect.spawn(f'hub pull-request --base vatlab/sos-docs:master -m "sos-docs updated from sos live server"',
-        child.logfile = sys.stdout, cwd=DIR)
+        cwd=DIR)
     while True:
         prompt = child.expect([
-            "github.com username: ",
-            "github.com password for sosdocs (never stored): ",
+            "github.com username:\s*",
+            r"github.com password for sosdocs \(never stored\):\s*",
             pexpect.EOF]
         )
         if prompt == 0:
             child.sendline('sosdocs')
         elif prompt == 1:
-            child.sendline('PASSWORD')
+            child.sendline('wxsAjvtkX5nu57m0')
         elif prompt == 2:
+            print(child.before.decode())
             child.close()
             break
     print(child.before.decode())
@@ -47,7 +51,7 @@ if __name__ == '__main__':
     DIR = '/home/jovyan/sos-docs'
     BRANCH = id_generator(6)
 
-    print('Syncing with upstream master...')
+    print('\nSyncing with upstream master...\n')
     subprocess.call('git fetch upstream master', cwd=DIR, shell=True)
     subprocess.call(f'git checkout -b {BRANCH} FETCH_HEAD', cwd=DIR, shell=True)
 
