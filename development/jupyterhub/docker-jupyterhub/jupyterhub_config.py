4343a351f10afe66569a8f56d789b0a0e630d1b5
Bo Peng <bpeng@mdanderson.org>
2019-02-22 12:18:33 -0600

    Enable cull_idle_severs service

--- development/jupyterhub/docker-jupyterhub/jupyterhub_config.py
+++ development/jupyterhub/docker-jupyterhub/jupyterhub_config.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 
 c.JupyterHub.authenticator_class = 'tmpauthenticator.TmpAuthenticator'
 
@@ -30,10 +31,10 @@ c.JupyterHub.active_server_limit = 20
 
 
 # kill idle server after a while
-# c.JupyterHub.services = [
-#     {
-#         'name': 'cull-idle',
-#         'admin': True,
-#         'command': [sys.executable, 'cull_idle_servers.py', '--timeout=3600'],
-#     }
-# ]
+c.JupyterHub.services = [
+     {
+         'name': 'cull-idle',
+         'admin': True,
+         'command': [sys.executable, 'cull_idle_servers.py', '--timeout=3600'],
+     }
+]
